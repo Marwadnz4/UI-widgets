dd97b044bbfc61dcd470580a36d6939ba15c79b4
Marwadnz4 <103198820+Marwadnz4@users.noreply.github.com>
2022-07-17 14:56:03 +0300

    My second app

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellowAccent,
+        body: SafeArea(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/gril.jpg'),
+              ),
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: const Text(
+                  'Marwa Mohammed',
+                  style: TextStyle(
+                      fontFamily: 'Akaya',
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 35,
+                    color: Colors.black,
+                  ),
+                  title: Text('Marwa.gmail.com',
+                      style: TextStyle(
+                        fontSize: 25,
+                      )),
+                ),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
